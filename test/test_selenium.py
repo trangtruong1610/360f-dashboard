efd4f215681918b0860dd4c870452a23e243b3cf
Trang Truong <trangtruong1610@gmail.com>
2020-05-06 22:33:08 +0700

    read input from in excel file to search

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -1,5 +1,9 @@
+import os
 import unittest
 from webdriver_docker import *
+from main.main import load_input_from_xlsx
+
+file_path = os.path.abspath(os.pardir)
 
 def setUpModule():    pass  # nothing here for now
 def tearDownModule(): pass  # nothing here for now
@@ -14,5 +18,7 @@ class Test(unittest.TestCase):
     def tearDown(self): self.driver.quit()  # nothing here for now
 
     def test_01(self):
+        excel = f'{file_path}/fixture/test_fixture.xlsx'
+        test_case = load_input_from_xlsx(excel)
         search_input = self.driver.find_element_by_name('q')
-        search_input.send_keys('selenium')
+        search_input.send_keys(test_case[1])
