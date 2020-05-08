a8553576fb92d9d3a2d22cc3016db8a3791380a7
nam.vu <nam.vu@360f.com>
2020-05-08 16:25:34 +0700

    simpler code

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -1,24 +1,19 @@
 import os
-import unittest
 from webdriver_docker import *
+
 from main.main import load_input_from_xlsx
 
-file_path = os.path.abspath(os.pardir)
 
-def setUpModule():    pass  # nothing here for now
-def tearDownModule(): pass  # nothing here for now
+class Test:
 
-
-class Test(unittest.TestCase):
-
-    def setUp(self):
+    def test_01(self):
         self.driver = loadWebDriver_chrome()
         self.driver.get('https://www.google.com/')
 
-    def tearDown(self): self.driver.quit()  # nothing here for now
-
-    def test_01(self):
+        file_path = os.path.abspath(os.pardir)
         excel = f'{file_path}/fixture/test_fixture.xlsx'
         test_case = load_input_from_xlsx(excel)
         search_input = self.driver.find_element_by_name('q')
         search_input.send_keys(test_case[1])
+
+        self.driver.quit()  # nothing here for now
