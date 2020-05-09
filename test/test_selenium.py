1b028ef18bc55261d3745b1c5a52ec3c639bf689
nam.vu <nam.vu@360f.com>
2020-05-09 23:05:31 +0700

    simpler code

--- test/test_selenium.py
+++ test/test_selenium.py
@@ -7,13 +7,13 @@ from main.main import load_input_from_xlsx
 class Test:
 
     def test_01(self):
-        self.driver = loadWebDriver_chrome()
-        self.driver.get('https://www.google.com/')
+        wd = loadWebDriver_chrome()
+        wd.get('https://www.google.com/')  # wd aka webdriver
 
         file_path = os.path.abspath(__file__ + '/../../')
         excel = f'{file_path}/fixture/test_fixture.xlsx'
         test_case = load_input_from_xlsx(excel)
-        search_input = self.driver.find_element_by_name('q')
+        search_input = wd.find_element_by_name('q')
         search_input.send_keys(test_case[1])
 
-        self.driver.quit()  # nothing here for now
+        wd.quit()  # nothing here for now
