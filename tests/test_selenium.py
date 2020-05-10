d9052b4cd1209130fc11f323d8673f262746e82c
namgivu <namgivu@gmail.com>
2020-05-10 16:35:10 +0700

    saving result of each testcase

--- tests/test_selenium.py
+++ tests/test_selenium.py
@@ -1,22 +1,30 @@
 import os
 from webdriver_docker import *
 
-from main.main import load_input_from_xlsx
+from src.service.util import load_input_from_xlsx
 
 
 class Test:
 
     def test_01(self):
-        wd = loadWebDriver_chrome()
-        wd.get('https://www.google.com/')  # wd aka webdriver
+        wd = loadWebDriver_chrome()  # wd aka webdriver
 
-        file_path = os.path.abspath(__file__ + '/../../')
-        excel = f'{file_path}/fixture/test_fixture.xlsx'
-        test_case = load_input_from_xlsx(excel)
-        search_input = wd.find_element_by_name('q')
-        search_input.send_keys(test_case[1])
+        TEST_HOME = os.path.abspath(__file__ + '/..'); testcase_excel = f'{TEST_HOME}/fixture/test_fixture.xlsx'
+        test_case_all = load_input_from_xlsx(testcase_excel)
 
-            #TODO Trang take snapshot of the search result here and save to snapshot/
+        r_all = []  # r_all aka all_results
+        for test_case_id, keyword in test_case_all.enumerate():
+            wd.get('https://www.google.com/')
+            search_input = wd.find_element_by_name('q')
+            search_input.send_keys(keyword)
+
+            #TODO Trang take snapshot of the search result here and save to snapshot/YYYYmmDD-HHMMSSz.png aka :snapshot_f
+
+            r = {  # r aka testcase result
+                'test_case_id': test_case_id,
+                'keyword'     : keyword,
+                'snapshot'    : 'TODO snapshot_f',
+            }; r_all.append(r)
 
         #TODO study to know quit() once or quit at every :tc end is faster
         wd.quit()  # nothing here for now
